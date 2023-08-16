27f602f5d8408f0fbb80038a07186fe73321f0f9
ayaz2110 <95169673+ayaz2110@users.noreply.github.com>
2023-08-16 20:58:20 +0100

    Add files via upload

---
+++ C177.py
@@ -0,0 +1,16 @@
+class score():
+    def __init__(self):
+        self.__score = 1
+        
+    def showscore(self):
+        print(self.__score)
+        
+    def update_score(self):
+        self.__score = self.__score + 1
+        print(self.__score)
+        
+player = score()
+player.score = 100
+player.update_score()
+player.update_score()        
+
